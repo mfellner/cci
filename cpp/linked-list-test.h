39f6b3a76a6e1e443b90398b51aa698e93658753
Maximilian Fellner <max.fellner@gmail.com>
2013-09-09 20:42:30 +0200

    Missing files.

---
+++ cpp/linked-list-test.h
@@ -0,0 +1,13 @@
+// Copyright 2013, Maximilian Fellner
+
+#ifndef LINKED_LIST_TEST_H_
+#define LINKED_LIST_TEST_H_
+
+#include "linked-list.h"
+#include "gtest/gtest.h"
+
+TEST(LinkedListTest, Add) {
+  EXPECT_EQ(1, 1);
+}
+
+#endif  // LINKED_LIST_TEST_H_
