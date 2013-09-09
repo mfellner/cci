c69f2927240d78b43133e350053916b3066b149b
Maximilian Fellner <max.fellner@gmail.com>
2013-09-10 01:21:04 +0200

    Implemented some linked list methods.

--- cpp/linked-list-test.h
+++ cpp/linked-list-test.h
@@ -5,9 +5,86 @@
 
 #include "linked-list.h"
 #include "gtest/gtest.h"
+#include <string>
+
+using namespace mfellner;
 
 TEST(LinkedListTest, Add) {
-  EXPECT_EQ(1, 1);
+  LinkedList<int> list;
+
+  EXPECT_EQ(0, list.Size());
+
+  int a = 1, b = 2, c = 3;
+  list.Add(&a);
+  list.Add(&b);
+  list.Add(&c);
+
+  EXPECT_EQ(3, list.Size());
+}
+
+TEST(LinkedListTest, GetFirst) {
+  LinkedList<int> list;
+
+  EXPECT_EQ(0, list.GetFirst());
+
+  int a = 1, b = 2, c = 3;
+  list.Add(&a);
+  list.Add(&b);
+  list.Add(&c);
+
+  int k = *list.GetFirst();
+  EXPECT_EQ(1, k);
+}
+
+TEST(LinkedListTest, GetLast) {
+  LinkedList<int> list;
+
+  EXPECT_EQ(0, list.GetLast());
+
+  int a = 1, b = 2, c = 3;
+  list.Add(&a);
+  list.Add(&b);
+  list.Add(&c);
+
+  int k = *list.GetLast();
+  EXPECT_EQ(3, k);
+}
+
+TEST(LinkedListTest, Clear) {
+  LinkedList<int> list;
+
+  int a = 1, b = 2;
+  list.Add(&a);
+  list.Add(&b);
+
+  EXPECT_EQ(2, list.Size());
+
+  list.Clear();
+
+  EXPECT_EQ(0, list.Size());
+  EXPECT_EQ(0, list.GetFirst());
+  EXPECT_EQ(0, list.GetLast());
+}
+
+TEST(LinkedListTest, ClearDoesNotDeleteData) {
+  LinkedList<std::string> list;
+
+  std::string stack_string = "Hello, World!";
+  list.Add(&stack_string);
+  list.Clear();
+
+  EXPECT_EQ(0, list.Size());
+  EXPECT_EQ("Hello, World!", stack_string);
+
+  std::string* heap_string = new std::string("Good day to you!");
+  list.Add(heap_string);
+  list.Clear();
+
+  EXPECT_EQ(0, list.Size());
+  EXPECT_EQ("Good day to you!", *heap_string);
+
+  delete heap_string;
+  heap_string = 0;
 }
 
 #endif  // LINKED_LIST_TEST_H_
--- cpp/linked-list.h
+++ cpp/linked-list.h
@@ -3,42 +3,124 @@
 #ifndef LINKED_LIST_H_
 #define LINKED_LIST_H_
 
+#include <stdlib.h>
+#include <exception>
+
 namespace mfellner {
 
 template <class E>
 class LinkedList {
  public:
+    LinkedList() : head_(0) {}
+
+   ~LinkedList() {
+     ListElement* tail = head_;
+     head_ = 0;
+
+     while (tail) {
+       ListElement* element = tail;
+       tail = tail->next;
+       delete element;
+       element = 0;
+     }
+   }
+
    // Appends the specified element to the end of this list.
-   bool Add(E* e);
+   void Add(E* e) {
+     ListElement* element = new ListElement();
+     element->next = 0;
+     element->data = e;
+
+     if (head_) {
+       ListElement* tail = head_;
+       while (tail->next) {
+         tail = tail->next;
+       }
+       tail->next = element;
+     } else {
+       head_ = element;
+     }
+   }
 
    // Inserts the specified element at the specified position in this list.
-   void Add(int index, E* e);
-   
+   void Add(int index, E* e) {
+     throw std::exception();
+   }
+
    // Inserts the specified element at the beginning of this list.
-   void AddFirst(E* e);
-   
+   void AddFirst(E* e) {
+     throw std::exception();
+   }
+
    // Appends the specified element to the end of this list.
-   void AddLast(E* e);
-   
+   void AddLast(E* e) {
+     throw std::exception();
+   }
+
    // Removes all of the elements from this list.
-   void Clear();
-   
+   void Clear() {
+     ListElement* tail = head_;
+     head_ = 0;
+
+     while (tail) {
+       ListElement* element = tail;
+       tail = tail->next;
+       delete element;
+       element = 0;
+     }
+   }
+
    // Returns the element at the specified position in this list.
-   E* Get(int index);
-   
+   E* Get(int) {
+     throw std::exception();
+   }
+
    // Returns the first element in this list.
-   E* GetFirst();
-   
+   E* GetFirst() {
+     return head_ ? head_->data : 0;
+   }
+
    // Returns the last element in this list.
-   E* GetLast();
+   E* GetLast() {
+     if (!head_) return 0;
+
+     ListElement* tail = head_;
+
+     while (tail->next) {
+       tail = tail->next;
+     }
+     return tail->data;
+   }
+
+   // Returns the number of elements in this list.
+   int Size() {
+     int size = 0;
+     ListElement* tail = head_;
+
+     while (tail) {
+       tail = tail->next;
+       size++;
+     }
+     return size;
+   }
 
  private:
+   // No copies.
+   LinkedList(const LinkedList& l) {}
+
    // An element in the linked list.
    typedef struct ListElement {
-     E* next;
+     ListElement* next;
      E* data;
    } ListElement;
+
+   ListElement* head_;
 };
+
+// template <class E>
+// void LinkedList<E>::Add(E* e) {
+//   throw std::exception();
+// }
 
 }
 
