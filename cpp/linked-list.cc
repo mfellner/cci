c5076158a90b9525c2af668905b99338fe0a85b9
Maximilian Fellner <max.fellner@gmail.com>
2013-09-09 17:53:55 +0200

    Added cpp files.

---
+++ cpp/linked-list.cc
@@ -0,0 +1,36 @@
+// Copyright 2013, Maximilian Fellner
+
+#include "linked-list.h"
+#include <iostream>
+ 
+namespace mfellner {
+
+  bool LinkedList::Add(E e) {
+      return false;
+  }
+
+  void LinkedList::Add(int index, E* e) {
+  }
+
+  void LinkedList::AddFirst(E* e) {
+  }
+
+  void LinkedList::AddLast(E* e) {
+  }
+
+  void LinkedList::Clear() {
+  }
+
+  E* LinkedList::Get(int index) {
+      return NULL;
+  }
+
+  E* LinkedList::GetFirst() {
+      return NULL;
+  }
+
+  E* LinkedList::GetLast() {
+      return NULL;
+  }
+
+}
---
+++ cpp/linked-list.h
@@ -0,0 +1,45 @@
+// Copyright 2013, Maximilian Fellner
+
+#ifndef LINKED_LIST_H_
+#define LINKED_LIST_H_
+
+namespace mfellner {
+
+template <typename E>
+class LinkedList {
+ public:
+   // Appends the specified element to the end of this list.
+   bool Add(E* e);
+
+   // Inserts the specified element at the specified position in this list.
+   void Add(int index, E* e);
+
+   // Inserts the specified element at the beginning of this list.
+   void AddFirst(E* e);
+
+   // Appends the specified element to the end of this list.
+   void AddLast(E* e);
+
+   // Removes all of the elements from this list.
+   void Clear();
+
+   // Returns the element at the specified position in this list.
+   E* Get(int index);
+
+   // Returns the first element in this list.
+   E* GetFirst();
+
+   // Returns the last element in this list.
+   E* GetLast();
+
+ private:
+   // An element in the linked list.
+   typedef struct ListElement {
+     E* next;
+     E* data;
+   } ListElement;
+};
+
+}
+
+#endif  // LINKED_LIST_H_
