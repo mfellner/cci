c7c54f66b0f4508d71bdabed8f3ed07572fc9a1a
Maximilian Fellner <max.fellner@gmail.com>
2013-09-11 15:31:20 +0200

    Methods for linked list.

--- cpp/linked-list-test.h
+++ cpp/linked-list-test.h
@@ -5,14 +5,21 @@
 
 #include "linked-list.h"
 #include "gtest/gtest.h"
+#include <exception>
 #include <string>
 
 using namespace mfellner;
 
+TEST(LinkedListTest, New) {
+  LinkedList<int> list;
+  EXPECT_EQ(0, list.Size());
+}
+
+////////////////////////////////////////
+// Add
+////////////////////////////////////////
 TEST(LinkedListTest, Add) {
   LinkedList<int> list;
-
-  EXPECT_EQ(0, list.Size());
 
   int a = 1, b = 2, c = 3;
   list.Add(&a);
@@ -20,36 +27,386 @@ TEST(LinkedListTest, Add) {
   list.Add(&c);
 
   EXPECT_EQ(3, list.Size());
+  EXPECT_EQ(1, *list.Get(0));
+  EXPECT_EQ(2, *list.Get(1));
+  EXPECT_EQ(3, *list.Get(2));
 }
 
+////////////////////////////////////////
+// Add Index
+////////////////////////////////////////
+TEST(LinkedListTest, AddIndexFront) {
+  LinkedList<int> list;
+
+  int a = 1, b = 2, c = 3;
+
+  list.Add(0, &a);
+  list.Add(0, &b);
+  list.Add(0, &c);
+
+  EXPECT_EQ(3, list.Size());
+  EXPECT_EQ(3, *list.Get(0));
+  EXPECT_EQ(2, *list.Get(1));
+  EXPECT_EQ(1, *list.Get(2));
+}
+
+TEST(LinkedListTest, AddIndexBack) {
+  LinkedList<int> list;
+
+  int a = 1, b = 2, c = 3;
+
+  list.Add(0, &a);
+  list.Add(1, &b);
+  list.Add(2, &c);
+
+  EXPECT_EQ(3, list.Size());
+  EXPECT_EQ(1, *list.Get(0));
+  EXPECT_EQ(2, *list.Get(1));
+  EXPECT_EQ(3, *list.Get(2));
+}
+
+TEST(LinkedListTest, AddIndexMiddle) {
+  LinkedList<int> list;
+
+  int a = 1, b = 2, c = 3;
+
+  list.Add(0, &a);
+  list.Add(1, &b);
+  list.Add(1, &c);
+
+  EXPECT_EQ(3, list.Size());
+  EXPECT_EQ(1, *list.Get(0));
+  EXPECT_EQ(3, *list.Get(1));
+  EXPECT_EQ(2, *list.Get(2));
+}
+
+TEST(LinkedListTest, AddIndexShouldThrow) {
+  LinkedList<int> list;
+
+  int a = 1, b = 2;
+  bool failed = false;
+
+  try {
+    list.Add(1, &a);
+  } catch(std::exception& e) {
+    failed = true;
+  }
+
+  EXPECT_TRUE(failed);
+  failed = false;
+
+  list.Add(0, &a);
+
+  try {
+    list.Add(-1, &b);
+  } catch(std::exception& e) {
+    failed = true;
+  }
+
+  EXPECT_TRUE(failed);
+  failed = false;
+
+  try {
+    list.Add(2, &b);
+  } catch(std::exception& e) {
+    failed = true;
+  }
+
+  EXPECT_TRUE(failed);
+}
+
+////////////////////////////////////////
+// Add First
+////////////////////////////////////////
+TEST(LinkedListTest, AddFirst) {
+  LinkedList<int> list;
+
+  int a = 1, b = 2, c = 3, d = 4;
+  list.AddFirst(&a);
+  list.AddFirst(&b);
+  list.AddFirst(&c);
+  list.AddFirst(&d);
+
+  EXPECT_EQ(4, *list.Get(0));
+  EXPECT_EQ(3, *list.Get(1));
+  EXPECT_EQ(2, *list.Get(2));
+  EXPECT_EQ(1, *list.Get(3));
+}
+
+////////////////////////////////////////
+// Add Last
+////////////////////////////////////////
+TEST(LinkedListTest, AddLast) {
+  LinkedList<int> list;
+
+  int a = 1, b = 2, c = 3, d = 4;
+  list.AddLast(&a);
+  list.AddLast(&b);
+  list.AddLast(&c);
+  list.AddLast(&d);
+
+  EXPECT_EQ(1, *list.Get(0));
+  EXPECT_EQ(2, *list.Get(1));
+  EXPECT_EQ(3, *list.Get(2));
+  EXPECT_EQ(4, *list.Get(3));
+}
+
+////////////////////////////////////////
+// Get Index
+////////////////////////////////////////
+TEST(LinkedListTest, GetIndex) {
+  LinkedList<int> list;
+
+  int a = 1, b = 2, c = 3;
+  list.Add(&a);
+  list.Add(&b);
+  list.Add(&c);
+
+  EXPECT_EQ(1, *list.Get(0));
+  EXPECT_EQ(2, *list.Get(1));
+  EXPECT_EQ(3, *list.Get(2));
+}
+
+TEST(LinkedListTest, GetIndexString) {
+  LinkedList<std::string> list;
+
+  std::string a = "Hello,";
+  list.Add(&a);
+  std::string b = "wonderful";
+  list.Add(&b);
+  std::string c = "world!";
+  list.Add(&c);
+
+  EXPECT_EQ("Hello,",    *list.Get(0));
+  EXPECT_EQ("wonderful", *list.Get(1));
+  EXPECT_EQ("world!",    *list.Get(2));
+}
+
+TEST(LinkedListTest, GetIndexShouldThrow) {
+  LinkedList<int> list;
+  bool failed = false;
+
+  try {
+    list.Get(0);
+  } catch(std::exception& e) {
+    failed = true;
+  }
+
+  EXPECT_TRUE(failed);
+  failed = false;
+
+  try {
+    list.Get(-1);
+  } catch(std::exception& e) {
+    failed = true;
+  }
+
+  EXPECT_TRUE(failed);
+  failed = false;
+
+  int a = 1;
+  list.Add(&a);
+
+  try {
+    list.Get(1);
+  } catch(std::exception& e) {
+    failed = true;
+  }
+
+  EXPECT_TRUE(failed);
+}
+
+////////////////////////////////////////
+// Get First
+////////////////////////////////////////
 TEST(LinkedListTest, GetFirst) {
   LinkedList<int> list;
 
-  EXPECT_EQ(0, list.GetFirst());
-
   int a = 1, b = 2, c = 3;
   list.Add(&a);
   list.Add(&b);
   list.Add(&c);
 
-  int k = *list.GetFirst();
-  EXPECT_EQ(1, k);
+  EXPECT_EQ(1, *list.GetFirst());
 }
 
+TEST(LinkedListTest, GetFirstShouldThrow) {
+  LinkedList<int> list;
+  bool failed = false;
+
+  try {
+    list.GetFirst();
+  } catch(std::exception& e) {
+    failed = true;
+  }
+
+  EXPECT_TRUE(failed);
+}
+
+////////////////////////////////////////
+// Get Last
+////////////////////////////////////////
 TEST(LinkedListTest, GetLast) {
   LinkedList<int> list;
 
-  EXPECT_EQ(0, list.GetLast());
+  int a = 1, b = 2, c = 3;
+  list.Add(&a);
+  list.Add(&b);
+  list.Add(&c);
+
+  EXPECT_EQ(3, *list.GetLast());
+}
+
+TEST(LinkedListTest, GetLastShouldThrow) {
+  LinkedList<int> list;
+  bool failed = false;
+
+  try {
+    list.GetLast();
+  } catch(std::exception& e) {
+    failed = true;
+  }
+
+  EXPECT_TRUE(failed);
+}
+
+////////////////////////////////////////
+// Get Pointers
+////////////////////////////////////////
+TEST(LinkedListTest, GetPointersAreEqual) {
+  LinkedList<int> list;
+
+  int a = 1;
+  list.Add(&a);
+
+  EXPECT_EQ(*list.GetFirst(), *list.GetLast());
+  EXPECT_EQ(list.GetFirst(), list.GetLast());
+  EXPECT_EQ(list.GetFirst(), list.Get(0));
+}
+
+////////////////////////////////////////
+// Remove Index
+////////////////////////////////////////
+TEST(LinkedListTest, RemoveIndexFront) {
+  LinkedList<int> list;
 
   int a = 1, b = 2, c = 3;
   list.Add(&a);
   list.Add(&b);
   list.Add(&c);
 
-  int k = *list.GetLast();
-  EXPECT_EQ(3, k);
+  EXPECT_EQ(1, *list.Remove(0));
+  EXPECT_EQ(2, list.Size());
+
+  EXPECT_EQ(2, *list.Remove(0));
+  EXPECT_EQ(1, list.Size());
+
+  EXPECT_EQ(3, *list.Remove(0));
+  EXPECT_EQ(0, list.Size());
 }
 
+TEST(LinkedListTest, RemoveIndexBack) {
+  LinkedList<int> list;
+
+  int a = 1, b = 2, c = 3;
+  list.Add(&a);
+  list.Add(&b);
+  list.Add(&c);
+
+  EXPECT_EQ(3, *list.Remove(2));
+  EXPECT_EQ(2, list.Size());
+
+  EXPECT_EQ(2, *list.Remove(1));
+  EXPECT_EQ(1, list.Size());
+
+  EXPECT_EQ(1, *list.Remove(0));
+  EXPECT_EQ(0, list.Size());
+}
+
+TEST(LinkedListTest, RemoveIndexMiddle) {
+  LinkedList<int> list;
+
+  int a = 1, b = 2, c = 3;
+  list.Add(&a);
+  list.Add(&b);
+  list.Add(&c);
+
+  EXPECT_EQ(2, *list.Remove(1));
+  EXPECT_EQ(2, list.Size());
+
+  EXPECT_EQ(3, *list.Remove(1));
+  EXPECT_EQ(1, list.Size());
+
+  EXPECT_EQ(1, *list.Remove(0));
+  EXPECT_EQ(0, list.Size());
+}
+
+TEST(LinkedListTest, RemoveIndexShouldThrow) {
+  LinkedList<int> list;
+  bool failed = false;
+
+  try {
+    list.Remove(0);
+  } catch(std::exception& e) {
+    failed = true;
+  }
+
+  EXPECT_TRUE(failed);
+  failed = false;
+
+  try {
+    list.Remove(-1);
+  } catch(std::exception& e) {
+    failed = true;
+  }
+
+  EXPECT_TRUE(failed);
+  failed = false;
+
+  int a = 1;
+  list.Add(&a);
+
+  try {
+    list.Remove(1);
+  } catch(std::exception& e) {
+    failed = true;
+  }
+
+  EXPECT_TRUE(failed);
+}
+
+////////////////////////////////////////
+// Remove Element
+////////////////////////////////////////
+TEST(LinkedListTest, RemoveElement) {
+  LinkedList<int> list;
+
+  int a = 1, b = 2, c = 3;
+  list.Add(&a);
+  list.Add(&b);
+
+  EXPECT_TRUE(list.Remove(&a));
+  EXPECT_EQ(1, list.Size());
+
+  EXPECT_TRUE(list.Remove(&b));
+  EXPECT_EQ(0, list.Size());
+
+  EXPECT_FALSE(list.Remove(&c));
+  EXPECT_EQ(0, list.Size());
+
+  list.Add(&a);
+  list.Add(&c);
+
+  EXPECT_TRUE(list.Remove(&c));
+  EXPECT_EQ(1, list.Size());
+
+  EXPECT_EQ(1, *list.GetFirst());
+  EXPECT_EQ(1, *list.GetLast());
+}
+
+////////////////////////////////////////
+// Clear
+////////////////////////////////////////
 TEST(LinkedListTest, Clear) {
   LinkedList<int> list;
 
@@ -62,29 +419,25 @@ TEST(LinkedListTest, Clear) {
   list.Clear();
 
   EXPECT_EQ(0, list.Size());
-  EXPECT_EQ(0, list.GetFirst());
-  EXPECT_EQ(0, list.GetLast());
 }
 
-TEST(LinkedListTest, ClearDoesNotDeleteData) {
-  LinkedList<std::string> list;
 
-  std::string stack_string = "Hello, World!";
-  list.Add(&stack_string);
-  list.Clear();
-
+////////////////////////////////////////
+// Size
+////////////////////////////////////////
+TEST(LinkedListTest, ListSize) {
+  LinkedList<int> list;
   EXPECT_EQ(0, list.Size());
-  EXPECT_EQ("Hello, World!", stack_string);
-
-  std::string* heap_string = new std::string("Good day to you!");
-  list.Add(heap_string);
-  list.Clear();
-
   EXPECT_EQ(0, list.Size());
-  EXPECT_EQ("Good day to you!", *heap_string);
 
-  delete heap_string;
-  heap_string = 0;
+  int a = 1;
+  list.Add(&a);
+  EXPECT_EQ(1, list.Size());
+  EXPECT_EQ(1, list.Size());
+
+  list.Clear();
+  EXPECT_EQ(0, list.Size());
+  EXPECT_EQ(0, list.Size());
 }
 
 #endif  // LINKED_LIST_TEST_H_
--- cpp/linked-list.h
+++ cpp/linked-list.h
@@ -4,7 +4,10 @@
 #define LINKED_LIST_H_
 
 #include <stdlib.h>
-#include <exception>
+#include <stdexcept>
+#include <iostream>
+#include <sstream>
+#include <string>
 
 namespace mfellner {
 
@@ -27,9 +30,7 @@ class LinkedList {
 
    // Appends the specified element to the end of this list.
    void Add(E* e) {
-     ListElement* element = new ListElement();
-     element->next = 0;
-     element->data = e;
+     ListElement* element = NewListElement(e);
 
      if (head_) {
        ListElement* tail = head_;
@@ -44,17 +45,55 @@ class LinkedList {
 
    // Inserts the specified element at the specified position in this list.
    void Add(int index, E* e) {
-     throw std::exception();
+     if (index < 0)
+       throw std::out_of_range(StringWithInt("invalid index ", index));
+
+     ListElement* element = NewListElement(e);
+
+     if (head_ && index > 0) {
+       ListElement* before = head_;
+       for (int i = 1; i < index; i++) {
+         before = before->next;
+         if ((!before && i < index) || (!before->next && i + 1 < index))
+           throw std::out_of_range(StringWithInt("invalid index ", index));
+       }
+       element->next = before->next;
+       before->next = element;
+     } else if (head_ && index == 0) {
+       element->next = head_;
+       head_ = element;
+     } else if (!head_ && index == 0) {
+       head_ = element;
+     } else {
+       throw std::out_of_range(StringWithInt("invalid index ", index));
+     }
    }
 
    // Inserts the specified element at the beginning of this list.
    void AddFirst(E* e) {
-     throw std::exception();
+     ListElement* element = NewListElement(e);
+
+     if (head_) {
+       element->next = head_;
+       head_ = element;
+     } else {
+       head_ = element;
+     } 
    }
 
    // Appends the specified element to the end of this list.
    void AddLast(E* e) {
-     throw std::exception();
+     ListElement* element = NewListElement(e);
+
+     if (head_) {
+       ListElement* tail = head_;
+       while (tail->next) {
+         tail = tail->next;
+       }
+       tail->next = element;
+     } else {
+       head_ = element;
+     } 
    }
 
    // Removes all of the elements from this list.
@@ -71,25 +110,94 @@ class LinkedList {
    }
 
    // Returns the element at the specified position in this list.
-   E* Get(int) {
-     throw std::exception();
+   E* Get(int index) {
+     if (index < 0)
+       throw std::out_of_range(StringWithInt("invalid index ", index));
+
+     if (head_ && index > 0) {
+       ListElement* element = head_;
+       for (int i = 1; i <= index; i++) {
+         element = element->next;
+         if (!element)
+           throw std::out_of_range(StringWithInt("invalid index ", index));
+       }
+       return element->data;
+     } else if (head_ && index == 0) {
+       return head_->data;
+     } else {
+       throw std::out_of_range(StringWithInt("invalid index ", index));
+     }
    }
 
    // Returns the first element in this list.
    E* GetFirst() {
-     return head_ ? head_->data : 0;
+     if (!head_) throw std::out_of_range("no such element");
+     return head_->data;
    }
 
    // Returns the last element in this list.
    E* GetLast() {
-     if (!head_) return 0;
+     if (!head_) throw std::out_of_range("no such element");
 
      ListElement* tail = head_;
-
      while (tail->next) {
        tail = tail->next;
      }
      return tail->data;
+   }
+
+   // Removes the element at the specified position in this list.
+   E* Remove(int index) {
+     if (index < 0)
+       throw std::out_of_range(StringWithInt("invalid index ", index));
+   
+     if (head_ && index > 0) {
+       ListElement* element = head_;
+       ListElement* before  = 0;
+       for (int i = 1; i <= index; i++) {
+         before  = element;
+         element = element->next;
+         if (!element)
+           throw std::out_of_range(StringWithInt("invalid index ", index));
+       }
+       ListElement* next = element->next;
+       E* data           = element->data;
+       delete element;
+       before->next      = next;
+       return data;
+     } else if (head_ && index == 0) {
+       ListElement* next = head_->next;
+       E* data           = head_->data;
+       delete head_;
+       head_             = next;
+       return data;
+     } else {
+       throw std::out_of_range(StringWithInt("invalid index ", index));
+     }
+   }
+
+   // Removes the first occurrence of the specified element from this list, if it is present.
+   bool Remove(E* e) {
+     if (head_ && *(head_->data) == *e) {
+       ListElement* next = head_->next;
+       delete head_;
+       head_ = next;
+       return true;
+     }
+
+     ListElement* element = head_;
+     ListElement* before  = 0;
+
+     while (element) {
+       if (*(element->data) == *e) {
+         before->next = element->next;
+         delete element;
+         return true;
+       }
+       before  = element;
+       element = element->next;
+     }
+     return false;
    }
 
    // Returns the number of elements in this list.
@@ -105,9 +213,6 @@ class LinkedList {
    }
 
  private:
-   // No copies.
-   LinkedList(const LinkedList& l) {}
-
    // An element in the linked list.
    typedef struct ListElement {
      ListElement* next;
@@ -115,12 +220,23 @@ class LinkedList {
    } ListElement;
 
    ListElement* head_;
-};
 
-// template <class E>
-// void LinkedList<E>::Add(E* e) {
-//   throw std::exception();
-// }
+   // No copies.
+   LinkedList(const LinkedList& l) {}
+
+   ListElement* NewListElement(E* e) {
+     ListElement* element = new ListElement();
+     element->next = 0;
+     element->data = e;
+     return element;
+   }
+
+   std::string StringWithInt(std::string s, int i) {
+     std::stringstream stream;
+     stream << s << i;
+     return stream.str();
+   }
+};
 
 }
 
